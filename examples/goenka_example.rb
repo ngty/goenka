f8cec3d6ac4488bd5f4110fbe9fdf58573c86cc3
NgTzeYang <ngty77@gmail.com>
2010-09-26 06:13:30 +0800

    Hide the always failing example.

--- examples/goenka_example.rb
+++ examples/goenka_example.rb
@@ -2,6 +2,6 @@ require 'example_helper'
 
 describe "Goenka" do
   it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+    #fail "hey buddy, you should probably rename this file and start specing for real"
   end
 end
