388e7c8fbce1d9db86462b032709429ac79d2214
NgTzeYang <ngty77@gmail.com>
2010-09-26 15:50:21 +0800

    Fixed path issue (plus wip support for pushing).

--- lib/goenka.rb
+++ lib/goenka.rb
@@ -1,18 +1,26 @@
 require 'nokogiri'
 require 'yaml'
-require 'curb'
-
-require 'goenka/configurable'
-require 'goenka/selectable'
-require 'goenka/hacks'
-require 'goenka/album'
+require 'flickraw-cached'
 
 module Goenka
 
+  class << self
+    def require_rb(*args)
+      require File.join(File.dirname(__FILE__), 'goenka', *args)
+    end
+  end
+
+  require_rb 'configurable'
+  require_rb 'selectable'
+  require_rb 'pushable'
+  require_rb 'album'
+  require_rb 'hacks'
+
   extend Configurable
   extend Selectable
+  extend Pushable
 
-  configure
+  configure # preliminary configuration when app inits
 
 end
 
