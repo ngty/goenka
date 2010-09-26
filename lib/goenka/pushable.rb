39928d6bf7152f55cda526455c44786ea263640e
NgTzeYang <ngty77@gmail.com>
2010-09-26 18:33:35 +0800

    Hurray, basic upload is done.

--- lib/goenka/pushable.rb
+++ lib/goenka/pushable.rb
@@ -6,9 +6,9 @@ module Goenka
 
     def push(album)
       authenticate!
-      select(album).map(&:to_requests).flatten.each do |requests|
-        photo = requests.delete(:photo)
-        flickr.upload_photo(photo, requests)
+      select(album).map(&:to_requests).flatten.each do |request|
+        photo = request.delete(:photo)
+        flickr.upload_photo(photo, request)
       end
     end
 
@@ -16,23 +16,20 @@ module Goenka
       FlickRaw.api_key = flickr_api_key
       FlickRaw.shared_secret = flickr_shared_secret
 
-      unless @auth_token && flickr.auth.checkToken(:auth_token => @auth_token)
-        frob = flickr.auth.getFrob
-        auth_url = FlickRaw.auth_url(:frob => frob, :perms => 'write')
+      frob = flickr.auth.getFrob
+      auth_url = FlickRaw.auth_url(:frob => frob, :perms => 'write')
 
-          puts "Open this url in your process to complete the authication process : #{auth_url}"
-          puts "Press Enter when you are finished."
-          STDIN.getc
+      # TODO: use mechanize (if possible) to avoid toggling to browser !!
+      puts "Open this url in your process to complete the authication process : #{auth_url}"
+      puts "Press Enter when you are finished."
+      STDIN.getc
 
-        begin
-          @auth_token = flickr.auth.getToken(:frob => frob)
-          flickr.test.login
-        rescue FlickRaw::FailedResponse => e
-          @auth_token = nil
-          raise FlickrAuthenticateError.new(e.msg)
-        end
+      begin
+        flickr.auth.getToken(:frob => frob)
+        flickr.test.login
+      rescue FlickRaw::FailedResponse => e
+        raise FlickrAuthenticateError.new(e.msg)
       end
-
     end
 
   end
